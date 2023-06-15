74e7713765da00020f5aa58af019700187f4e428
Stanislav Yanakiev <stan5050@gmail.com>
2023-06-15 13:17:50 +0300

    'updated with new solutions'

--- all_solutions.py
+++ all_solutions.py
@@ -318,4 +318,59 @@ def migratory_birds(arr):
 
 
 print(migratory_birds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]))
+
+
 ################################################################
+# julian and gregorian calendars
+def day_of_programmer(year):
+    days_of_first_8_moths = 243
+
+    if year > 1918 and (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)):
+        days_of_first_8_moths += 1
+    elif year == 1918:
+        days_of_first_8_moths -= 13
+    elif year < 1918 and year % 4 == 0:
+        days_of_first_8_moths += 1
+
+    the_day_of_programmer = 256 - days_of_first_8_moths
+
+    if len(str(the_day_of_programmer)) == 1:
+        return f'0{the_day_of_programmer}.09.{year}'
+    return f'{the_day_of_programmer}.09.{year}'
+
+
+print(day_of_programmer(1918))
+
+
+################################################################
+
+# julian and gregorian calendars with functions
+
+def is_leap_gregorian(year):
+    if year > 1918 and (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)):
+        return True
+
+
+def is_leap_julian(year):
+    if year < 1918 and year % 4 == 0:
+        return True
+
+
+def day_of_programmer(year):
+    days_of_first_8_moths = 243
+
+    if is_leap_gregorian(year) or is_leap_julian(year):
+        days_of_first_8_moths += 1
+
+    elif year == 1918:
+        days_of_first_8_moths -= 13
+
+    the_day_of_programmer = 256 - days_of_first_8_moths
+
+    if len(str(the_day_of_programmer)) == 1:
+        return f'0{the_day_of_programmer}.09.{year}'
+    return f'{the_day_of_programmer}.09.{year}'
+
+
+print(day_of_programmer(2016))
+################################
